1129661896297b96a1a5e421f20f42dca2716aae
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-05 08:49:32 -0300

    Renaming main spring boot application

--- task/src/test/java/com/project/task/StartUpTests.java
+++ task/src/test/java/com/project/task/StartUpTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskApplicationTests {
+class StartUpTests {
 
 	@Test
 	void contextLoads() {
