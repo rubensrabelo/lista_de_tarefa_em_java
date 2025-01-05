3171960724f186dfc6e6573fbf07cd8551fc3601
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-05 09:01:51 -0300

    Adding models

---
+++ task/src/main/java/com/project/task/models/Task.java
@@ -0,0 +1,116 @@
+package com.project.task.models;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_task")
+public class Task implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String title;
+	
+	@Column(nullable = false)
+	private boolean isCompleted;
+	
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+	
+	public Task() {
+	}
+	
+	public Task(String title) {
+		this.title = title;
+		this.isCompleted = false;
+	}
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
+		this.isCompleted = false;
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(createdAt, id, isCompleted, title, updatedAt);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		return Objects.equals(createdAt, other.createdAt) && Objects.equals(id, other.id)
+				&& isCompleted == other.isCompleted && Objects.equals(title, other.title)
+				&& Objects.equals(updatedAt, other.updatedAt);
+	}
+}
