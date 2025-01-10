aec79c4478b023acdcb077b5f7c90d35626200e7
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-10 07:52:05 -0300

    Adding a Task constructor to TaskDTO

--- task/src/main/java/com/project/task/dto/TaskDTO.java
+++ task/src/main/java/com/project/task/dto/TaskDTO.java
@@ -1,4 +1,10 @@
 package com.project.task.dto;
 
+import com.project.task.models.Task;
+
 public record TaskDTO(Long id, String title,  boolean isCompleted) {
+
+	public TaskDTO(Task task) {
+		this(task.getId(), task.getTitle(), task.getCompleted());
+	}
 }
