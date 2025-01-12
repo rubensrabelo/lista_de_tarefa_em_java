164f887ba8e6fc5d7c857d773be87924d5b06efd
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-12 10:32:14 -0300

    Editing resource exception to accept user error

--- task/src/main/java/com/project/task/services/exceptions/ResourceNotFoundException.java
+++ task/src/main/java/com/project/task/services/exceptions/ResourceNotFoundException.java
@@ -4,6 +4,10 @@ public class ResourceNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 	
+	public ResourceNotFoundException(String email) {
+		super(email + " not found.");
+	}
+	
 	public ResourceNotFoundException(String type, Long id) {
 		super(type + " not with id=" + id + " found.");
 	}
