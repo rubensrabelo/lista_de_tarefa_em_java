f667e6cb7e66d0099b0b1dc7766e6206f40d3c9f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-14 11:55:47 -0300

    Ending auth controller

--- task/src/main/java/com/project/task/controllers/AuthController.java
+++ task/src/main/java/com/project/task/controllers/AuthController.java
@@ -1,11 +1,24 @@
 package com.project.task.controllers;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.project.task.dto.user.LoginRequestDTO;
+import com.project.task.dto.user.RegisterRequestDTO;
+import com.project.task.dto.user.ResponseDTO;
 import com.project.task.infra.security.TokenService;
+import com.project.task.models.User;
 import com.project.task.repositories.UserRepository;
+import com.project.task.services.exceptions.ResourceNotFoundException;
 
+@RestController
+@RequestMapping(value = "/auth")
 public class AuthController {
 	
 	private final UserRepository repository;
@@ -18,11 +31,33 @@ public class AuthController {
 		this.tokenService = tokenService;
 	}
 	
-	public ResponseEntity<T> login() {
-		return null;
+	@PostMapping("/login")
+	public ResponseEntity<ResponseDTO> login(@RequestBody LoginRequestDTO userLogin) {
+		User user = repository.findByEmail(userLogin.email())
+				.orElseThrow(() -> new ResourceNotFoundException(userLogin.email()));
+		
+		if(passwordEnconder.matches(userLogin.password(), user.getPassword())) {
+			String token = this.tokenService.generateToken(user);
+			ResponseDTO response = new ResponseDTO(user.getName(), token);
+			return ResponseEntity.ok().body(response);
+		}
+		
+		return ResponseEntity.badRequest().build();
 	}
 	
-	public ResponseEntity<T> register() {
-		return null;
+	public ResponseEntity<ResponseDTO> register(@RequestBody RegisterRequestDTO userRegister) {
+		Optional<User> user = this.repository.findByEmail(userRegister.email());
+		
+		if(user.isEmpty()) {
+			User newUser = new User();
+			newUser.setPassword(passwordEnconder.encode(userRegister.password()));
+			newUser.setEmail(userRegister.email());
+			newUser.setName(userRegister.name());
+			String token = this.tokenService.generateToken(newUser);
+			ResponseDTO response = new ResponseDTO(newUser.getName(), token);
+			return ResponseEntity.ok().body(response);
+		}
+		
+		return ResponseEntity.badRequest().build();
 	}
 }
