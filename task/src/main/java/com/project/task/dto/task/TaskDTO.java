5f15c68bd8ae1c869882e70d41ce035a18befdac
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-17 14:43:43 -0300

    Fixing code

--- task/src/main/java/com/project/task/dto/task/TaskDTO.java
+++ task/src/main/java/com/project/task/dto/task/TaskDTO.java
@@ -5,6 +5,6 @@ import com.project.task.models.Task;
 public record TaskDTO(Long id, String title,  boolean isCompleted) {
 
 	public TaskDTO(Task task) {
-		this(task.getId(), task.getTitle(), task.getCompleted());
+		this(task.getId(), task.getTitle(), task.isCompleted());
 	}
 }
