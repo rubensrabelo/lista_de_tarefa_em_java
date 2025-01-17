84a058f2a409a2308b881c0cfce9c7dbeff34abc
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-17 15:09:06 -0300

    Adding athentication in the service method

--- task/src/main/java/com/project/task/services/TaskService.java
+++ task/src/main/java/com/project/task/services/TaskService.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import com.project.task.dto.task.TaskUpdateData;
 import com.project.task.models.Task;
+import com.project.task.models.User;
 import com.project.task.repositories.TaskRepository;
+import com.project.task.repositories.UserRepository;
 import com.project.task.services.exceptions.DatabaseException;
 import com.project.task.services.exceptions.ResourceNotFoundException;
 
@@ -18,25 +21,36 @@ public class TaskService {
 
 	@Autowired
 	private TaskRepository repository;
+	
+	@Autowired
+	private UserRepository userRepository;
 
-	public List<Task> findAll() {
-		List<Task> tasks = repository.findAll();
+	public List<Task> findAllTasksByUser(UserDetails authenticatedUser) {
+		User user = getAuthenticatedUser(authenticatedUser);
+		List<Task> tasks = repository.findByUserId(user.getId());
 		return tasks;
 	}
 
-	public Task findById(Long id) {
-		Task task = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Task", id));
+	public Task findTaskByUser(Long id, UserDetails authenticatedUser) {
+		User user = getAuthenticatedUser(authenticatedUser);
+		Task task = repository.findByIdAndUserId(id, user.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("Task", id));
 		return task;
 	}
 
-	public Task create(Task task) {
+	public Task create(Task task, UserDetails authenticatedUser) {
+		User user = getAuthenticatedUser(authenticatedUser);
+		task.setUser(user);
 		task = repository.save(task);
 		return task;
 	}
 
-	public void delete(Long id) {
+	public void delete(Long id, UserDetails authenticatedUser) {
 		try {
-			repository.deleteById(id);
+			User user = getAuthenticatedUser(authenticatedUser);
+			Task task = repository.findByIdAndUserId(id, user.getId())
+					.orElseThrow(() -> new ResourceNotFoundException("Task", id));
+			repository.delete(task);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException("Task", id);
 		} catch (DataIntegrityViolationException e) {
@@ -44,8 +58,9 @@ public class TaskService {
 		}
 	}
 
-	public Task update(Long id, TaskUpdateData taskUpdate) {
-		Task task = repository.findById(id)
+	public Task update(Long id, TaskUpdateData taskUpdate, UserDetails authenticatedUser) {
+		User user = getAuthenticatedUser(authenticatedUser);
+		Task task = repository.findByIdAndUserId(id, user.getId())
 				.orElseThrow(() -> new ResourceNotFoundException("Task", id));
 		updateData(task, taskUpdate);
 		task = repository.save(task);
@@ -55,5 +70,10 @@ public class TaskService {
 	private void updateData(Task task, TaskUpdateData taskUpdate) {
 		task.setTitle(taskUpdate.title());
 		task.setCompleted(taskUpdate.isCompleted());
+	}
+	
+	private User getAuthenticatedUser(UserDetails authenticatedUser) {
+		return userRepository.findByEmail(authenticatedUser.getUsername())
+				.orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
 	}
 }
