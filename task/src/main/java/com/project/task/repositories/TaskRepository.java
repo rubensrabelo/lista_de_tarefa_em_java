3986e2effd2b106b62b69a076db77786e4dad25f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-17 15:08:38 -0300

    Adding to find a task by user method in the task repository

--- task/src/main/java/com/project/task/repositories/TaskRepository.java
+++ task/src/main/java/com/project/task/repositories/TaskRepository.java
@@ -1,8 +1,15 @@
 package com.project.task.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.task.models.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+	List<Task> findByUserId(Long userId);
+
+	Optional<Task> findByIdAndUserId(Long taskId, Long userId);
 }
