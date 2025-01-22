93cd3ae93470c0687479215f5f76a4ba7df695ad
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-22 10:23:28 -0300

    Adding isActive atribute in the task dto

--- task/src/main/java/com/project/task/dto/task/TaskDTO.java
+++ task/src/main/java/com/project/task/dto/task/TaskDTO.java
@@ -2,9 +2,9 @@ package com.project.task.dto.task;
 
 import com.project.task.models.Task;
 
-public record TaskDTO(Long id, String title,  boolean isCompleted) {
+public record TaskDTO(Long id, String title,  boolean isCompleted, boolean isActive) {
 
 	public TaskDTO(Task task) {
-		this(task.getId(), task.getTitle(), task.isCompleted());
+		this(task.getId(), task.getTitle(), task.isCompleted(), task.isActive());
 	}
 }
